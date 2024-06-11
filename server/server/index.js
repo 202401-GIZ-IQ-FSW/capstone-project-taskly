a1cce5b21b476c486fbb57a79072bfbf4a098869
Ninos Dinkha <ninos.dinkha97@gmail.com>
2024-06-11 23:03:01 +0300

    add temp admin routes

--- server/server/index.js
+++ server/server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const projectRoutes = require('./routes/project/project')
+const projectRoutes = require('./routes/project/project');
 
 require('dotenv').config();
 
@@ -8,6 +8,7 @@ const connectToMongo = require('./db/connection');
 const cookieParser = require('cookie-parser');
 // routes
 const authRoutes = require('./routes/auth/auth');
+const adminRoutes = require('./routes/admin/admin');
 const ticketRoutes = require('./routes/tickets/ticket');
 const verifyJWT = require('./middleware/verifyJWT');
 
@@ -19,16 +20,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
-
-
 app.use('/api/v1/auth', authRoutes);
-// routes for project crud
-
+app.use('/api/v1/admin', adminRoutes); // temporary admin routes
 
 app.use(verifyJWT); // everything below this line will use verifyJWT
 app.use('/api/v1/projects/:projectId/tickets', ticketRoutes);
 app.use('/api/v1/projects', projectRoutes);
-
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
