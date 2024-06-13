c9bbfa13ca57932a2fe37cfb6d3a61d48ffd23ed
Ninos Dinkha <ninos.dinkha97@gmail.com>
2024-06-13 22:22:44 +0300

    use camelCase for util + organize server imports

--- server/server/index.js
+++ server/server/index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-const projectRoutes = require('./routes/project/project');
-const userRoutes = require('./routes/user/userProfileRoute');
-const {printallroutesintheconsole} = require("./util/devHelpers");
-
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
+// middleware and utils
 const connectToMongo = require('./db/connection');
-const cookieParser = require('cookie-parser');
+const { printAllRoutes } = require('./util/devHelpers');
+const verifyJWT = require('./middleware/verifyJWT');
+
 // routes
 const authRoutes = require('./routes/auth/auth');
 const adminRoutes = require('./routes/admin/admin');
+const projectRoutes = require('./routes/project/project');
+const userRoutes = require('./routes/user/userProfileRoute');
 const ticketRoutes = require('./routes/tickets/ticket');
-const verifyJWT = require('./middleware/verifyJWT');
 
 const app = express();
 const port = process.env.NODE_ENV === 'test' ? process.env.NODE_LOCAL_TEST_PORT : process.env.NODE_LOCAL_PORT;
@@ -25,6 +26,7 @@ app.use(cookieParser());
 app.get('/test', (req, res) => {
   res.json('Server connection to client works!!  Good Luck with your capstones :D');
 });
+
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/admin', adminRoutes); // temporary admin routes
 
@@ -34,10 +36,9 @@ app.use('/api/v1/projects', projectRoutes);
 app.use('/api/v1/projects/:projectId/tickets', ticketRoutes);
 
 app.listen(port, () => {
-  printallroutesintheconsole(app);
+  printAllRoutes(app);
   console.log(`Server listening on port ${port}`);
   connectToMongo();
 });
-
 
 module.exports = app;
