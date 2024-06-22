a1074b9a1936585b3db7861af4fa32416cda474c
Ahmedly SAM <ahmed.esam@duc.edu.iq>
2024-06-22 09:38:42 +0300

    add on ticket view, add ticketDetailes component, add start script to root package.json, add postinstall to client package.json,change login button to input

--- client/src/app/auth/login/page.jsx
+++ client/src/app/auth/login/page.jsx
@@ -86,11 +86,11 @@ export default function Login() {
             Keep me signed in
           </label>
         </div>
-        <button
+        <input
           type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-          Sign In
-        </button>
+          value="Sign In"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        />
       </form>
     </div>
   );
