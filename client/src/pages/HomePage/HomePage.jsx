82cf0b83d37765cd0735e429322b4c7b1adf2271
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-06-28 20:35:58 +0300

    -importing the notification component in the home, and store the user id

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,32 @@
+import React, { useEffect, useState } from 'react';
 import TicketStats from '../../components/Tickets/TicketStats';
 import TicketSubmission from '../../components/Tickets/TicketSubmission';
+import Notifications from '../../components/Notifications/notification';
+import axios from 'axios';
 
 const Home = () => {
+  //to store the user's ID
+  const [userId, setUserId] = useState(null);
+ //  an asynchronous function that fetches the user profile from the backend.
+//   The user ID is extracted from the response and stored in the userId state.
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      try {
+        const response = await axios.get('/api/v1/user/profile');
+        const userId = response.data._id;
+        setUserId(userId);
+      } catch (error) {
+        console.error('Error fetching user profile:', error);
+      }
+    };
+
+    fetchUserProfile();
+  }, []);
+
+  if (!userId) {
+    return <div>Loading...</div>; // we can replace it with any loading tool
+  }
+
   return (
     <>
       <TicketSubmission
@@ -16,6 +41,10 @@ const Home = () => {
         resolved="500K+"
         supportText="Get help now"
       />
+      <div>
+        <h1>Notifications</h1>
+        <Notifications userId={userId} />
+      </div>
     </>
   );
 };
