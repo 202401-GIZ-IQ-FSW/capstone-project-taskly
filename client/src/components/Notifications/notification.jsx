c3b38d175fc5d1de096862d2a4c3412fa491766d
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-06-28 20:42:25 +0300

    use client

--- client/src/components/Notifications/notification.jsx
+++ client/src/components/Notifications/notification.jsx
@@ -1,3 +1,6 @@
+'use client'
+
+
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import axios from 'axios';
--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useEffect, useState } from 'react';
 import TicketStats from '../../components/Tickets/TicketStats';
 import TicketSubmission from '../../components/Tickets/TicketSubmission';
