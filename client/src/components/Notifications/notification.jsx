acce4a16c43153cef3a32e25f1ead68a8b71edc2
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-06-29 16:26:51 +0300

    fix: use fetcher to handle request instead of axios

--- client/src/components/Notifications/notification.jsx
+++ client/src/components/Notifications/notification.jsx
@@ -1,13 +1,10 @@
 'use client'
 
-
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
-import axios from 'axios';
+import { fetcher } from '../../_utils/fetcher';
 
 const Notifications = ({ userId }) => {
-
-    // intiliaze state and socket io
   const [notifications, setNotifications] = useState([]);
   const socket = io('http://localhost:3000'); 
 
@@ -15,8 +12,8 @@ const Notifications = ({ userId }) => {
     // Fetch existing notifications
     const fetchNotifications = async () => {
       try {
-        const response = await axios.get('/api/v1/notifications');
-        setNotifications(response.data);
+        const data = await fetcher('/api/v1/notifications');
+        setNotifications(data);
       } catch (error) {
         console.error('Error fetching notifications:', error);
       }
@@ -28,22 +25,26 @@ const Notifications = ({ userId }) => {
     socket.on('connect', () => {
       console.log('Connected to Socket.io server');
     });
-    //When a new notification is received, the callback function is executed.
+
+    // When a new notification is received, the callback function is executed.
     socket.on('new_notification', (notification) => {
       setNotifications((prevNotifications) => [notification, ...prevNotifications]);
     });
 
-    // Clean up for prevent memory leaking 
+    // Clean up to prevent memory leaking 
     return () => {
       socket.disconnect();
     };
   }, []);
- 
-  // a function to mark notification as read
+
+  // A function to mark notification as read
   const markAsRead = async (notificationId) => {
     try {
-      await axios.put(`/api/v1/notifications/${notificationId}`);
-      // mapping over existing notifications and update 'is_read' field for the matched notifications
+      await fetcher(`/api/v1/notifications/${notificationId}`, {
+        method: 'PUT',
+      });
+
+      // Map over existing notifications and update 'is_read' field for the matched notifications
       setNotifications((prevNotifications) =>
         prevNotifications.map((notif) =>
           notif._id === notificationId ? { ...notif, is_read: true } : notif
@@ -54,16 +55,14 @@ const Notifications = ({ userId }) => {
     }
   };
 
-  // rendering the component
+  // Rendering the component
   return (
     <div>
       <h2>Notifications</h2>
       <ul>
-        //Maps over the notifications state to create a list item for each notification.
         {notifications.map((notification) => (
           <li
             key={notification._id}
-            //Conditionally sets the background color of the list item based on whether the notification has been read.
             style={{ backgroundColor: notification.is_read ? 'white' : 'lightgray' }}
             onClick={() => markAsRead(notification._id)}
           >
@@ -73,5 +72,6 @@ const Notifications = ({ userId }) => {
       </ul>
     </div>
   );
-  
 };
+
+export default Notifications;
