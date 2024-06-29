044ffac9d001f096c3591528352e4a72f4785ac2
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-06-29 16:10:33 +0300

    fix: remove notification components from homepage

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,34 +1,8 @@
-'use client'
-
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import TicketStats from '../../components/Tickets/TicketStats';
 import TicketSubmission from '../../components/Tickets/TicketSubmission';
-import Notifications from '../../components/Notifications/notification';
-import axios from 'axios';
 
 const Home = () => {
-  //to store the user's ID
-  const [userId, setUserId] = useState(null);
- //  an asynchronous function that fetches the user profile from the backend.
-//   The user ID is extracted from the response and stored in the userId state.
-  useEffect(() => {
-    const fetchUserProfile = async () => {
-      try {
-        const response = await axios.get('/api/v1/user/profile');
-        const userId = response.data._id;
-        setUserId(userId);
-      } catch (error) {
-        console.error('Error fetching user profile:', error);
-      }
-    };
-
-    fetchUserProfile();
-  }, []);
-
-  if (!userId) {
-    return <div>Loading...</div>; // we can replace it with any loading tool
-  }
-
   return (
     <>
       <TicketSubmission
@@ -44,8 +18,7 @@ const Home = () => {
         supportText="Get help now"
       />
       <div>
-        <h1>Notifications</h1>
-        <Notifications userId={userId} />
+        <h1>Home Page</h1>
       </div>
     </>
   );
