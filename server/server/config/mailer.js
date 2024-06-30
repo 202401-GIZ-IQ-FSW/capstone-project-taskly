c5d5ee4e84deaeae867c9579a63145c58dd45be3
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-06-30 11:20:24 +0300

    built: set nodemailer configration

---
+++ server/server/config/mailer.js
@@ -0,0 +1,23 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth:{
+        user: process.env.USER,
+        pass: process.env.PASSWORD
+    },
+
+});
+
+const sendEmail = (to, subject, text) => {
+    const info = {
+        from: process.env.USER,
+        to,
+        subject,
+        text,
+    };
+    return transporter.sendEmail(info);
+};
+module.exports = sendEmail;
