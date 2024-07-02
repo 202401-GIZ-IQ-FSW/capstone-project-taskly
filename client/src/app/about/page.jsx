c744d7a5dc1307193c00b0c4380edb84b89e961c
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-07-02 14:36:40 +0300

    fix some issue with images path!

--- client/src/app/about/page.jsx
+++ client/src/app/about/page.jsx
@@ -1,36 +1,39 @@
 'use client';
-const imagesPath = '../../../public/images/';
-import RecodedImage from  '../../../public/images/recoded.png'
+import React from "react";
+
+  // team data for links and images 
+const teamMembers = [
+  {
+    name: 'Ahmed Essam',
+    imageUrl: '/Ahmed.jpg',
+    linkedinUrl: 'https://www.linkedin.com/in/ahmed-essam-m/',
+  },
+  {
+    name: 'Ninos Dinkha',
+    imageUrl: '/Ninos.jpg',
+    linkedinUrl: 'https://www.linkedin.com/in/ninosdinkha/',
+  },
+  {
+    name: 'Elaf Gardi',
+    imageUrl: '/Elaf.jpg',
+    linkedinUrl: 'https://www.linkedin.com/in/elaf-ghassan/',
+  },
+  {
+    name: 'Meer Atta',
+    imageUrl: '/Meer.jpg',
+    linkedinUrl: 'https://www.linkedin.com/in/meer-atta-b825b4229/',
+  },
+  {
+    name: 'Ali Izz-aldin',
+    imageUrl: '/Ali.jpg',
+    linkedinUrl: 'https://www.linkedin.com/in/ali-izz-aldin-406975298/',
+  },
+];
+
  
 const About = () => {
   // team data for links and images 
-  const teamMembers = [
-    {
-      name: 'Ahmed Essam',
-      imageUrl: require(`${imagesPath}Ahmed.jpg`),
-      linkedinUrl: 'https://www.linkedin.com/in/ahmed-essam-m/',
-    },
-    {
-      name: 'Ninos Dinkha',
-      imageUrl: require(`${imagesPath}Ninos.jpg`),
-      linkedinUrl: 'https://www.linkedin.com/in/ninosdinkha/',
-    },
-    {
-      name: 'Elaf Gardi',
-      imageUrl: require(`${imagesPath}Elaf.jpg`),
-      linkedinUrl: 'https://www.linkedin.com/in/elaf-ghassan/',
-    },
-    {
-      name: 'Meer Atta',
-      imageUrl: require(`${imagesPath}Meer.jpg`),
-      linkedinUrl: 'https://www.linkedin.com/in/meer-atta-b825b4229/',
-    },
-    {
-      name: 'Ali Izz-aldin',
-      imageUrl: require(`${imagesPath}Ali.jpg`),
-      linkedinUrl: 'https://www.linkedin.com/in/ali-izz-aldin-406975298/',
-    },
-  ];
+  
   return (
     <div className="flex flex-col items-center p-6 md:p-12 bg-gradient-to-br from-blue-200 to-green-200" >
        <section className="max-w-4xl w-full text-center">
@@ -40,7 +43,7 @@ const About = () => {
         
         <div className="flex justify-center">
         <img 
-          src={image} 
+          src="/recoded.png" 
           alt="Team" 
           className="w-full max-w-md h-auto rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105" 
         />  
