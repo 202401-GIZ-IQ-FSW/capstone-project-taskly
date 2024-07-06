93dec8b57fd6271f9f06c28ec2c86f542786d6f8
Ahmedly SAM <ahmed.esam@duc.edu.iq>
2024-07-06 20:39:46 +0300

    add project managment page Fixes #79

--- server/server/index.js
+++ server/server/index.js
@@ -52,7 +52,7 @@ app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/projects', projectRoutes);
 app.use('/api/v1/projects', ticketRoutes);
 app.use(
-  '/api/v1/projects/:projectId/tickets/:ticketId/comments',
+  '/api/v1/projects',
   commentsRouter
 );
 app.use('/api/v1/dashboard', dashboardRoutes);
--- server/server/models/CommentModel.js
+++ server/server/models/CommentModel.js
@@ -1,54 +1,66 @@
+// server\server\models\CommentModel.js
 const mongoose = require('mongoose');
-const { Schema } = mongoose; 
+const { Schema } = mongoose;
 
-const replySchema = new Schema({
+const replySchema = new Schema(
+  {
     userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
     },
     content: {
-        type: String,
-        required: true
-    }
-}, 
-{ 
-    timestamps: true 
-});
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const commentSchema = new Schema({
+const commentSchema = new Schema(
+  {
     ticketId: {
-        type: Schema.Types.ObjectId,
-        ref: 'Ticket',
-        required: true,
+      type: Schema.Types.ObjectId,
+      ref: 'Ticket',
+      required: true,
     },
     commentedBy: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
     },
     content: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    replies: [replySchema] // Embedding reply schema to handle user replies to a comment
-}, 
-{ 
-    timestamps: true 
-});
+    replies: [replySchema], // Embedding reply schema to handle user replies to a comment
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // Pre-hook to populate replies and their user details before executing find queries
-commentSchema.pre("find", function(next) {
-    this.populate({
-        path: "replies",
-        populate: {
-            path: "commentedBy"
-        }
-    });
-    next();
+commentSchema.pre('find', function (next) {
+  this.populate({
+    path: 'replies',
+    populate: {
+      path: 'commentedBy',
+    },
+  });
+  next();
 });
+// Pre-hook for findOne queries as well
+commentSchema.pre('findOne', function (next) {
+    this.populate('commentedBy')
+        .populate({
+          path: 'replies.userId',
+        });
+    next();
+  });
 
- 
 const Comment = mongoose.model('Comment', commentSchema);
 
 module.exports = Comment;
