7ec4296f8626689702b2fe748fc5811ddfc4cdf0
Ninos Dinkha <ninos.dinkha97@gmail.com>
2024-07-09 22:59:07 +0300

    update layout to collapse sidebar

--- server/server/db/connection.js
+++ server/server/db/connection.js
@@ -1,28 +1,29 @@
 // server\server\db\connection.js
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, TEST_DB_HOST } =
   process.env;
 
 const DB_URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${
-  process.env.NODE_ENV === "test" ? TEST_DB_HOST : DB_HOST
+  process.env.NODE_ENV === 'test' ? TEST_DB_HOST : DB_HOST
 }:${DB_PORT}/${DB_NAME}?authSource=admin`;
 
 let url = DB_URI;
 // uncomment this line if you are using `nodemon server/index.js` command
 // url = 'mongodb://localhost:27017/tickets-db';
-mongoose.set("strictQuery", true);
+// mongodb+srv://ahmed2:0UHBOmgvr8YiULV6@cluster0.xnuvh3k.mongodb.net/
+mongoose.set('strictQuery', true);
 const connectToMongo = () => {
   mongoose.connect(url, { useNewUrlParser: true });
 
   db = mongoose.connection;
 
-  db.once("open", () => {
-    console.log("Database connected: ", url);
+  db.once('open', () => {
+    console.log('Database connected: ', url);
   });
 
-  db.on("error", (err) => {
-    console.error("Database connection error: ", err);
+  db.on('error', (err) => {
+    console.error('Database connection error: ', err);
   });
 };
 
