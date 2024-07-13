552a319848f9b8d5f53e38f4f930d25f1f164b03
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-07-13 10:18:41 +0300

    make a useEffect for request the data from server

--- client/src/components/analysis/verticalChart.jsx
+++ client/src/components/analysis/verticalChart.jsx
@@ -1,5 +1,6 @@
+import fetcher from "@/_utils/fetcher";
 import React, {useEffect, useState} from "react";
-
+import fetcher from "@/_utils/fetcher";
 import { Bar } from "react-chartjs-2";
 
 const VerticalChart = () => {
@@ -7,3 +8,21 @@ const VerticalChart = () => {
     const [allUsers, setAllUsers] = useState([]);
     const [allProjects, setAllProjects] = useState([]);
 }
+
+useEffect(() => {
+    const fetchData = async () => {
+        const ticketsResponse = await fetcher('/api/analytics/all-tickets');
+        const ticketsData = await ticketsResponse.json();
+        setAllTickets(ticketsData);
+
+        const usersResponse = await fetcher('/api/analytics/all-users');
+        const usersData = await usersResponse.json();
+        setAllUsers(usersData);
+
+        const projectsResponse = await fetcher('/api/analytics/all-projects');
+        const projectsData = await projectsResponse.json();
+        setAllProjects(projectsData);
+    };
+    fetchData();
+}, []);
+
