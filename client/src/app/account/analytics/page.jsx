6a722de9a74ab49ce23bab50d8c19ed6bfb55a55
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-07-13 11:12:25 +0300

    integrate the two Charts into the analytics component

--- client/src/app/account/analytics/page.jsx
+++ client/src/app/account/analytics/page.jsx
@@ -1,5 +1,12 @@
+import React from "react";
+import DoughnutChart from "@/components/analysis/doughnutChart";
+import VerticalChart from "@/components/analysis/verticalChart";
+
 const Analytics = () => {
-  return <div>Analytics</div>;
+  return <div> <VerticalChart />
+            <DoughnutChart />
+            </div>;
+
 };
 
 export default Analytics;
