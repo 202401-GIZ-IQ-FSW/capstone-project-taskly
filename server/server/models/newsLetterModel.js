c8edcb2c86eb6168546cb6ec45715f0d161b9074
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-07-13 16:56:15 +0300

    update the function and the model to handle reports instead of newsletters.

--- server/server/models/newsLetterModel.js
+++ server/server/models/newsLetterModel.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
-const { emit } = require('./UserModel');
 
-const newsLetterSchema = new mongoose.Schema({
-    email: {type: String, required: true},
-    subject: {type: String, required: true},
-    message: {type: String, required: true},
-    sentAt: {type: Date, default: Date.now}
+const reportSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    subject: { type: String, required: true },
+    message: { type: String, required: true },
+    sentAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('NewsletterSchema', newsLetterSchema);
+module.exports = mongoose.model('Report', reportSchema);
