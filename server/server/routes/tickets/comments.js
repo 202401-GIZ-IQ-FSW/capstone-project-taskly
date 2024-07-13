3ba691fcc39edcd648ca592c96cf40d44e9a6a73
Ninos Dinkha <ninos.dinkha97@gmail.com>
2024-07-13 19:53:37 +0300

    update comment model to not return sensitive data

--- server/server/routes/tickets/comments.js
+++ server/server/routes/tickets/comments.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 
 const commentController = require('../../controllers/comment/commentController');
+const { validateObjectId } = require('../../middleware/validateObjectId');
 
 // Base path: /api/v1/projects/tickets/id/comments
 
 router.post('/', commentController.createComment);
 router.get('/', commentController.getAllComments);
-router.get('/:commentId', commentController.getCommentById);
-router.put('/:commentId', commentController.updateCommentById);
-router.delete('/:commentId', commentController.deleteCommentById);
+router.get('/:commentId', validateObjectId('commentId'),commentController.getCommentById);
+router.put('/:commentId', validateObjectId('commentId'),commentController.updateCommentById);
+router.delete('/:commentId', validateObjectId('commentId'),commentController.deleteCommentById);
 
 module.exports = router;
