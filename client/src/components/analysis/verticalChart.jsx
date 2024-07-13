01d4536771a1fa9b525b16eb71107bc6fa767c4f
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-07-13 10:15:35 +0300

    intiliaze the verticalchart component, -install the demanded packages

---
+++ client/src/components/analysis/verticalChart.jsx
@@ -0,0 +1,9 @@
+import React, {useEffect, useState} from "react";
+
+import { Bar } from "react-chartjs-2";
+
+const VerticalChart = () => {
+    const [allTickets, setAllTickets] = useState([]);
+    const [allUsers, setAllUsers] = useState([]);
+    const [allProjects, setAllProjects] = useState([]);
+}
