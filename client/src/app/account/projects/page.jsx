3ddd33dcdc0bf841517fcc954d5826fe096e5499
Ninos Dinkha <ninos.dinkha97@gmail.com>
2024-07-15 12:05:48 +0300

    update projects

--- client/src/app/account/projects/page.jsx
+++ client/src/app/account/projects/page.jsx
@@ -1,37 +1,17 @@
 // client\src\app\account\projects\page.jsx
 'use client';
-import React, { useState, useEffect } from 'react';
-import ProjecstList from '@/components/Projects/ProjecstList';
+import CreateProjectModal from '@/components/Projects/CreateProjectModal';
 import ProjectSettings from '@/components/Projects/ProjectSettings';
 import TicketManagement from '@/components/Projects/TicketManagement';
-import CreateProjectModal from '@/components/Projects/CreateProjectModal';
-import fetcher from '@/_utils/fetcher';
+import { useProjects } from '@/context/ProjectsContext/ProjectsContext';
+import { useState } from 'react';
 
 const ProjectsPage = () => {
   const [projects, setProjects] = useState([]);
-  const [selectedProject, setSelectedProject] = useState(null);
+  const { selectedProject, setSelectedProject } = useProjects();
   const [error, setError] = useState(null);
   const [showSettingsDropdown, setShowSettingsDropdown] = useState(false);
   const [showCreateProjectModal, setShowCreateProjectModal] = useState(false);
-
-  useEffect(() => {
-    const fetchProjects = async () => {
-      try {
-        const response = await fetcher('/v1/projects');
-        setProjects(Array.isArray(response) ? response : []);
-        if (projects && projects.length != 0) {
-          window['projectSelect'].value = projects[0]._id;
-        }
-      } catch (err) {
-        setError(err.message);
-      }
-    };
-    fetchProjects();
-  }, []);
-  useEffect(() => {
-    projects ?? (window['projectSelect'].value = projects[0]._id);
-    setSelectedProject(projects[0]);
-  }, [projects]);
 
   const handleClicks = (event) => {
     if (!event.target.id.includes('settings')) {
@@ -46,11 +26,11 @@ const ProjectsPage = () => {
   return (
     <div className="p-4" onClick={handleClicks}>
       <div className="mb-4 flex justify-left items-center">
-        <ProjecstList
+        {/* <ProjecstList
           selectedProject={selectedProject}
           projects={projects}
           setSelectedProject={setSelectedProject}
-        />
+        /> */}
 
         <div className="flex flex-row justify-end w-full">
           {!selectedProject && (
@@ -81,16 +61,16 @@ const ProjectsPage = () => {
             selectedProject={selectedProject}
             setError={setError}
           />
-          <div className="mt-8">
+          {/* <div className="mt-8">
             <h2 className="text-2xl font-bold mb-4">Project Activity Log</h2>
-            {/* <ul>
+            <ul>
               {selectedProject.activityLog.map((activity, index) => (
                 <li key={index} className="mb-2">
                   {activity}
                 </li>
               ))}
-            </ul> */}
-          </div>
+            </ul>
+          </div> */}
         </div>
       )}
       {showCreateProjectModal && (
