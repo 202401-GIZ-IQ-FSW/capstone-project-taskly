4631ca5da058e7755cd66fd02136cb4f26ce2407
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-07-19 12:15:55 +0300

    update the callbackUrl of Google auth

--- server/server/config/passportConfig.js
+++ server/server/config/passportConfig.js
@@ -20,7 +20,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3001/api/v1/auth/google/callback',
+      callbackURL: 'https://tickets-backend-ahmedesam24.replit.app/api/v1/auth/google/callback',
       userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
     },
     async (accessToken, refreshToken, profile, cb) => {
